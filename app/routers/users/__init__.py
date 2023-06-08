34eaa00f3a968d750ccb5a9f09e89ccc62b5dfb0
moro23 <billhab66@gmail.com>
2023-06-08 18:28:25 +0000

    users subpackage.

---
+++ app/routers/users/__init__.py
@@ -0,0 +1,5 @@
+from routers.users.account.main import user_acc_router
+from routers.users.account.models import User, Administrator 
+
+from routers.users.user_type.main import usertype_router 
+from routers.users.user_type.models import UserType

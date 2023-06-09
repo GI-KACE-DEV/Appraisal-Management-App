78c074b37932c403aa49dd950a8cd69b5b4a6b2f
moro23 <billhab66@gmail.com>
2023-06-09 16:27:40 +0000

    added login urls.

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,8 @@ from fastapi import APIRouter , WebSocket, Depends
 from routers.users.account.main import user_acc_router
 from routers.users.user_type.main import usertype_router
 from routers.department_type.main import deptype_router 
-from routers.staffs.main import staff_router
+from routers.staffs.main import staff_router 
+from routers.users.auth.main import auth_router
 #from app.main import app
 import config as cfg
 
@@ -16,3 +17,4 @@ api_router.include_router(user_acc_router, tags=['User & Adminstrator Accounts']
 api_router.include_router(deptype_router, tags=['Department Type'])
 api_router.include_router(staff_router, tags=['Staff'])
 api_router.include_router(usertype_router, tags=['User Type'])
+api_router.include_router(auth_router, prefix="/login", tags=["login"])

937328b2af30c132b5c020fb26adb89da0d87731
moro23 <billhab66@gmail.com>
2023-06-12 14:16:21 +0000

    updated users auth

--- app/routers/users/auth/main.py
+++ app/routers/users/auth/main.py
@@ -1,8 +1,9 @@
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import Depends,APIRouter
+from fastapi import Depends,APIRouter, status,HTTPException
 from sqlalchemy.orm import Session
 from datetime import timedelta
-from fastapi import status,HTTPException
+
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import JWTError, jwt
 
 from db.session import get_db
 from core.hashing import Hasher
@@ -39,4 +40,24 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db: 
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+##
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+## lets create a function to identify a current user
+def get_current_user_from_token(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials", 
+    )
 
+    try:
+        payload = jwt.decode(token, settings.SECRETE_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        print("username/email extracted is", username)
+        if username is None:
+            raise credentials_exception
+    except JwTError:
+        raise credentials_exception
+    user = get_user(username=username, db=db)
+    if user is None:
+        raise credentials_exception 
+    return user 

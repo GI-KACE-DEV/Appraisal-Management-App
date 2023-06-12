5e14b5f14a6313b767437ad79560ad98635da096
moro23 <billhab66@gmail.com>
2023-06-12 15:12:19 +0000

    updated the database file.

--- app/database.py
+++ app/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
-from utils import db_url
 
-engine = create_engine(db_url()) #, connect_args={"check_same_thread": False}
+from core.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL) #, connect_args={"check_same_thread": False}
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base(metadata=MetaData(schema=None))
 

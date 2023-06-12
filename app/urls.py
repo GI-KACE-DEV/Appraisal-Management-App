8d03a84f9c90ac1893c1cda1cd25099486013c68
moro23 <billhab66@gmail.com>
2023-06-12 15:11:05 +0000

    updated urls file.

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter , WebSocket, Depends
 
 from routers.users.account.main import user_acc_router
 from routers.users.user_type.main import usertype_router
-from routers.department_type.main import deptype_router 
 from routers.staffs.main import staff_router 
 from routers.appraisal_form.main import appraisalForm_router 
 from routers.users.auth.main import auth_router
@@ -15,7 +14,6 @@ api_router = APIRouter()
 
 
 api_router.include_router(user_acc_router, tags=['User & Adminstrator Accounts'], prefix='/accounts')
-api_router.include_router(deptype_router, tags=['Department Type'])
 api_router.include_router(staff_router, tags=['Staff'], prefix="/staff")
 api_router.include_router(appraisalForm_router, tags=['AppraisalForm'], prefix="/appraisalForm")
 api_router.include_router(usertype_router, tags=['User Type'])

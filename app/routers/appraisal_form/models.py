1d944042432c0f9b8ed6e35634b46da6e7f67866
moro23 <billhab66@gmail.com>
2023-06-14 16:01:59 +0000

    updated the appraisal form model.

--- app/routers/appraisal_form/models.py
+++ app/routers/appraisal_form/models.py
@@ -7,14 +7,14 @@ from database import Base
 class AppraisalForm(Base):
     '''Appraisal Form Model'''
     
-    __tablename__ = "appraisal_form"
+    __tablename__ = "appraisal_forms"
     appraisal_form_id = Column(Integer,primary_key=True,index=True)
     department = Column(String, nullable=True)
     grade = Column(String, nullable=True)
     positions = Column(String, nullable=True)
     appraisal_date = Column(String, nullable=True)
-    staff_id = Column(Integer, ForeignKey("staff.staff_id"))
+    staff_id = Column(Integer, ForeignKey("staffs.staff_id"))
     status = Column(String, nullable=True)
-    created_at = Column(TIMESTAMP, nullable=False,server_default=text("CURRENT_TIMESTAMP"))
-    updated_at = Column(TIMESTAMP, nullable=False,server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
-    satff = relationship("Staff", back_populates="appraisal_form")
+    created_at = Column(Date, nullable=False)
+    updated_at = Column(Date, nullable=False)
+    staffs = relationship("Staff", back_populates="appraisal_forms")

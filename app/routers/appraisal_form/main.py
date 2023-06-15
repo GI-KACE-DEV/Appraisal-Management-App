e76eb123a4b21541f24eec7f531c42b9142ce06a
moro23 <billhab66@gmail.com>
2023-06-15 12:02:05 +0000

    added api route for creating an appraisal form.

--- app/routers/appraisal_form/main.py
+++ app/routers/appraisal_form/main.py
@@ -10,10 +10,31 @@ from  dependencies import get_db
 appraisalForm_router = APIRouter()
 
 
-@appraisalForm_router.post("/create", response_description="AppraisalForm data added into the database")
-async def create_new_appraisal_form(appraisalForm:schemas.CreateAppraisalForm, db:Session = Depends(get_db)):
+@appraisalForm_router.post("/create", response_model=schemas.ShowAppraisalForm)
+async def create_new_appraisal_form(appForm:schemas.CreateAppraisalForm, db:Session = Depends(get_db)):
+    current_staff_id = 1
+    return await crud.create_new_appraisal_form(appForm=appForm, db=db, staff_id=current_staff_id)
+
+
+
+
+
+
+
+# @appraisalForm_router.get("/getAppraisalForm")
+# async def get_all_appraisal_form(db:Session = Depends(get_db)):
+
+#     return await crud.get_all_appraisal_form(db)
+
+
+
+
+
+
+# @appraisalForm_router.get("/getAppraisalFormById/{id}")
+# async def getAppraisalFormById(id: int, db:Session = Depends(get_db)):
     
-    return await crud.create_new_appraisal_form(appraisalForm, db)
+#     return await crud.get_appraisal_formBy_ID(id, db)
 
 
 
@@ -21,20 +42,10 @@ async def create_new_appraisal_form(appraisalForm:schemas.CreateAppraisalForm, d
 
 
 
-@appraisalForm_router.get("/getAppraisalForm")
-async def get_all_appraisal_form(db:Session = Depends(get_db)):
-
-    return await crud.get_all_appraisal_form(db)
-
-
-
-
-
-
-@appraisalForm_router.get("/getAppraisalFormById/{id}")
-async def getAppraisalFormById(id: int, db:Session = Depends(get_db)):
+# @appraisalForm_router.put("/updateAppraisalForm")
+# async def updateAppraisalForm(updateStaff: schemas.UpdateAppraisalForm, db:Session = Depends(get_db)):
     
-    return await crud.get_appraisal_formBy_ID(id, db)
+#     return await crud.updateAppraisalForm(updateStaff, db)
 
 
 
@@ -42,19 +53,8 @@ async def getAppraisalFormById(id: int, db:Session = Depends(get_db)):
 
 
 
-@appraisalForm_router.put("/updateAppraisalForm")
-async def updateAppraisalForm(updateStaff: schemas.UpdateAppraisalForm, db:Session = Depends(get_db)):
+
+# @appraisalForm_router.delete("/deleteAppraisalForm/{id}")
+# async def deleteAppraisalForm(id: int, db:Session = Depends(get_db)):
     
-    return await crud.updateAppraisalForm(updateStaff, db)
-
-
-
-
-
-
-
-
-@appraisalForm_router.delete("/deleteAppraisalForm/{id}")
-async def deleteAppraisalForm(id: int, db:Session = Depends(get_db)):
-    
-    return await crud.deleteAppraisalForm(id, db)
+#     return await crud.deleteAppraisalForm(id, db)

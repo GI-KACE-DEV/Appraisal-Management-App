de7711c92006eb468eed5f4f6b3d489464fcd48a
moro23 <billhab66@gmail.com>
2023-06-23 16:21:25 +0000

    updated.

--- app/routers/users/__init__.py
+++ app/routers/users/__init__.py
@@ -5,4 +5,9 @@ from routers.users.user_type.main import usertype_router
 from routers.users.user_type.models import UserType
 
 from routers.users.auth.main import auth_router 
+from routers.users.auth.models import EmailVerificationCode, RevokedToken
+
+# from routers.users.role.models import Role, RolePermission 
+
+# from routers.users.permission.models import Permission, ContentType
 

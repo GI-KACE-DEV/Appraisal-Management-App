78662d2d94eb3310bc868032b265724d4fe5da98
moro23 <billhab66@gmail.com>
2023-06-23 16:20:23 +0000

    updated.

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,13 +2,26 @@ from routers.users import *
 
 from routers.staffs import *
 
+from routers.appraisal_form import *
+
+
 from database import Base, engine
 
-__all__ = [
-    'users',
-    'staffs',
-    'user_type',
-    'appraisal_forms'
-]
+# __all__ = [
+#     'Base',
+#     'account',
+#     'auth',
+#     'permission',
+#     'role',
+#     'staffs',
+#     'user_type',
+#     'appraisal_forms',
+#     'content_type',
+#     'deadline',
+#     'end_of_year',
+#     'mid_year_review',
+# ]
 #tables=[table for table in Base.metadata.sorted_tables if table.schema=='public' or table.schema=='global']
-Base.metadata.create_all(bind=engine) 
+#tables=[table for table in Base.metadata.sorted_tables if table.schema=='public' or table.schema=='global']
+#Base.metadata.create_all(bind=engine) 
+Base.metadata.create_all(bind=engine)

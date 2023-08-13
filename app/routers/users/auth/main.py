84691b93d290b4ac0cd08c477d189fdd1df498ab
moro23 <billhab66@gmail.com>
2023-08-13 10:27:04 +0000

    updated the auth file.

--- app/routers/users/auth/main.py
+++ app/routers/users/auth/main.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from datetime import timedelta
 
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from typing import Annotated
+
 from jose import JWTError, jwt
 
 from pydantic import EmailStr 
@@ -12,17 +14,29 @@ from dependencies import get_db
 from core.hashing import Hasher
 from .schemas import Token
 
-from .crud import authenticate_user
+from .crud import  get_user
 from core.security import create_access_token
 from core.config import settings
 from core.utils import create_jwt
 
 auth_router = APIRouter()
 
+async def authenticate_user(username: str, password: str, db: Session):
 
-@auth_router.post('/login', response_model=schemas.LoginResponse, name='Login')
-async def login_for_access_token(payload:schemas.Login, db:Session=Depends(get_db)):
-    user = await authenticate_user(payload, db)
+    user = get_user(email=username)
+    
+    if not user:
+        return False
+    
+            
+    if not Hasher.verify_password(password, user.hashed_password):
+        return False 
+
+    return user
+
+@auth_router.post('/token', response_model=schemas.LoginResponse, name='Login')
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db:Session=Depends(get_db)):
+    user = await authenticate_user(form_data.username, form_data.password, db)
 
     if not user:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="account is not active")
@@ -47,7 +61,31 @@ async def login_for_access_token(payload:schemas.Login, db:Session=Depends(get_d
     }
 
 
+## 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") 
+    
+    
+## lets create a function to create a dependency to indentify a current user
+async def get_current_user_from_token(token: str = Depends(oauth2_scheme), db: Session=Depends(get_db)):
+    
 
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials"
+    )
+    try:
+        #token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMUBleGFtcGxlLmNvbSIsImV4cCI6MTY5MTc1MzkxOX0.u0IcNwimQk_ctPldFEYibyDfX9JSuS4jYwNS8xzdEpo"
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        print("username/email extracted is ",username)
+    except JWTError:
+        raise credentials_exception
+    
+    user = get_user(username=username, db=db)
+
+    if user is None:
+        raise credentials_exception
+    return user 
 
 @auth_router.post("/logout", name='Logout')
 async def logout(payload:schemas.Logout, db:Session=Depends(get_db)):
